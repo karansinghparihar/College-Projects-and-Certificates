38dbea4dc72464d329378ede4bf051baa4503f66
karansinghparihar <941998karan@gmail.com>
2018-01-09 21:47:34 +0530

    GDB debugiing testfile 1

---
+++ testsss.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+int test(int a)
+{
+if(a<10)
+{
+printf(" less than 10 = %d\n",a);
+a++;
+return a;
+}
+return a;
+}
+void main()
+{
+int q=test(12);
+printf(" value of a = %d\n",q);}
